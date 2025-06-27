f6473d6adcd25a25b2bdfa3556e3d9fc751f14ca
enrique flores <enriquefft2001@gmail.com>
2025-06-27 01:53:34 -0500

    feat: reorganize source layout

---
+++ include/token/token.hpp
@@ -0,0 +1,17 @@
+#ifndef PASCAL_COMPILER_TOKENS_HPP
+#define PASCAL_COMPILER_TOKENS_HPP
+
+#include <string>
+
+namespace pascal {
+
+enum class TokenType { Identifier, Number, String, EndOfFile };
+
+struct Token {
+  TokenType type;
+  std::string lexeme;
+};
+
+} // namespace pascal
+
+#endif // PASCAL_COMPILER_TOKENS_HPP
